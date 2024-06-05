f6198a3718e52763d34283184168065053aec898
MyBook Hype <MyBook Hype@MyBookHype>
2024-06-05 14:18:42 +0700

    farel32

---
+++ src/main/java/com/mycompany/konversisuhu/Konversisuhu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.konversisuhu;
+
+/**
+ *
+ * @author MyBook Hype
+ */
+public class Konversisuhu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
